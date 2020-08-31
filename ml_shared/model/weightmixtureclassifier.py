d0783eaf29d38144fd82f79531e0d3ca38d873f3
Katagiri, Satoshi <katagiri.stsh@gmail.com>
2020-08-31 20:05:42 +0900

    scipy.sparse への対応

--- ml_shared/model/weightmixtureclassifier.py
+++ ml_shared/model/weightmixtureclassifier.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.sparse import csr_matrix, issparse
 from scipy.special import expit
 from sklearn.base import clone
 from sklearn.ensemble import BaggingClassifier
@@ -8,19 +9,30 @@ import warnings
 class WeightMixtureClassifier(BaggingClassifier):
   """
   引数: sklearn.ensemble.BaggingClassifier と同じ
-  Parameter mixing 用オブジェクト. coef_ を作るタイプの推定器をbaggingする際にパラメータの時点で平均化する
+  Parameter mixing 用オブジェクト. coef_ を作るタイプの推定器をbaggingする際にパラメータの時点で平均化する.
   ただしこの実装では Mann et al. (2009) が主張するようなパフォーマンスの恩恵を得られない
   binary classification しか対応していない
   参考:
   Mann, G. S., McDonald, R., Mohri, M., Silberman, N., & Walker, D. (2009). "Efficient large-scale distributed training of conditional maximum entropy models", Advances in neural information processing systems 22 (pp. 1231–1239). https://papers.nips.cc/paper/3881-efficient-large-scale-distributed-training-of-conditional-maximum-entropy-models
   """
+  def fit(self, X, y, **kwargs):
+    super().fit(X, y, **kwargs)
+    if hasattr(self.estimators_[0], 'intercept_'):
+      intercept = np.concatenate([est.intercept_ for est in self.estimators_]).mean()
+      self.intercept_ = intercept
+    return self
+
   def predict_proba(self, X):
     weights = np.concatenate([est.coef_ for est in self.estimators_]).mean(axis=0)
-    if self.base_estimator.get_params().get('fit_intercept'):
-      intercept = np.concatenate([est.intercept_ for est in self.estimators_]).mean()
+    if hasattr(self, 'intercept_'):
+      intercept = self.intercept_
     else:
       intercept = 0.0
-    p = expit(np.inner(X, weights) + intercept).reshape(-1, 1)
+    if issparse(X):
+      z = X.dot(weights)
+    else:
+      z = np.inner(X, weights)
+    p = expit(z + intercept).reshape(-1, 1)
     return np.concatenate((1-p, p), axis=1)
   
   def predict_log_proba(self, X):
