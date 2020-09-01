60cea493fafb4d18ded48a32b938d6ed2456f70a
Katagiri, Satoshi <katagiri.stsh@gmail.com>
2020-09-01 15:00:16 +0900

    ヘルプ修正

--- ml_shared/model/weightmixtureclassifier.py
+++ ml_shared/model/weightmixtureclassifier.py
@@ -9,9 +9,10 @@ import warnings
 class WeightMixtureClassifier(BaggingClassifier):
   """
   引数: sklearn.ensemble.BaggingClassifier と同じ
-  Parameter mixing 用オブジェクト. coef_ を作るタイプの推定器をbaggingする際にパラメータの時点で平均化する.
+  ただし, base_estimator は `coef_`, `intercept_` を持つ線形モデルにのみ対応
+  Parameter mixing 用オブジェクト. 基底推定器をbaggingではなくパラメータの時点で平均化する.
   ただしこの実装では Mann et al. (2009) が主張するようなパフォーマンスの恩恵を得られない
-  binary classification しか対応していない
+  現時点では binary classification しか対応していない
   参考:
   Mann, G. S., McDonald, R., Mohri, M., Silberman, N., & Walker, D. (2009). "Efficient large-scale distributed training of conditional maximum entropy models", Advances in neural information processing systems 22 (pp. 1231–1239). https://papers.nips.cc/paper/3881-efficient-large-scale-distributed-training-of-conditional-maximum-entropy-models
   """
