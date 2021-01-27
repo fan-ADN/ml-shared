2e364d3163b4726bf57c16b5a8338de71b40cd04
Katagiri, Satoshi <katagiri.stsh@gmail.com>
2021-01-27 21:54:11 +0900

    delete FM MCMC version

--- ml_shared/model/__init__.py
+++ ml_shared/model/__init__.py
@@ -1,7 +1,6 @@
 from ml_shared.model.fmwrapper import (
     FastFMALSClassifier,
     FastFMSGDClassifier,
-    FastFMMCMCClassifier
 )
 from ml_shared.model.imblearn_reacalibrator import (
     ImblearnRecalibrator
